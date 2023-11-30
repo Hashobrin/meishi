62dbe32018324f201957d1f1e900810d0bc05d9c
Fubii <hashobrin@gmail.com>
2023-11-30 08:23:53 +0000

    cron push

---
+++ api/entrypoint.sh
@@ -0,0 +1,11 @@
+docker-compose run --entrypoint dev-app \
+    "poetry run uvicorn api.main:app --host=0.0.0.0 --port=8000 --log-level trace --reload"
+
+# docker-compose run --entrypoint \
+#     "poetry init \
+#         --name demo-app \
+#         --dependency fastapi \
+#         --dependency uvicorn[standard]" \
+#     demo-app
+
+# docker-compose run --entrypoint "poetry install --no-root" demo-app
